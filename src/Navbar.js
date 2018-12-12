584021c4cf6b33f0a16f68f687a12ed37516ec77
Reece <Uni@Reeces-MacBook-Pro-Main.local>
2018-12-12 12:49:19 +1100

    linked with express server, setup player index

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  return(
+    <nav>
+      <a href="http://localhost:3000/">Home</a>
+      <a href="http://localhost:3000/teams/">Teams</a>
+      <a href="http://localhost:3000/players/">All Players</a>
+    </nav>
+  )
+}
+
+
+export default Navbar;
